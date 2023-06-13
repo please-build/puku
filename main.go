9f7603c1ff98b521b09a48c80d0b4c1c5b931ae7
Richard Godden <7768980+goddenrich@users.noreply.github.com>
2023-06-13 11:46:13 +0100

    added watch functionality (#16)
    Co-authored-by: rgodden <rgodden@thoughtmachine.net>

--- main.go
+++ main.go
@@ -6,12 +6,14 @@ import (
 	"github.com/peterebden/go-cli-init/v5/flags"
 	"github.com/peterebden/go-cli-init/v5/logging"
 
-	"github.com/please-build/puku/config"
 	"github.com/please-build/puku/generate"
+	"github.com/please-build/puku/watch"
+	"github.com/please-build/puku/work"
 )
 
 var opts = struct {
 	Usage string
+	Watch bool `long:"watch" description:"Watch the directory"`
 	Args  struct {
 		Paths []string `positional-arg-name:"packages" description:"The packages to process"`
 	} `positional-args:"true"`
@@ -24,7 +26,7 @@ puku is a tool used to generate and update Go targets in build files
 var log = logging.MustGetLogger()
 
 func main() {
-	root, err := config.FindRepoRoot()
+	root, err := work.FindRoot()
 	if err != nil {
 		log.Fatalf("%v", err)
 	}
@@ -33,14 +35,20 @@ func main() {
 		log.Fatalf("failed to set working dir to repo root: %v", err)
 	}
 	flags.ParseFlagsOrDie("puku", &opts, nil)
-	g := generate.NewUpdate("plz", "third_party/go")
+	u := generate.NewUpdate("plz", "third_party/go")
 
 	paths := opts.Args.Paths
 	if len(opts.Args.Paths) == 0 {
 		paths = []string{"..."}
 	}
+	if opts.Watch {
+		err := watch.Watch(u, paths...)
+		if err != nil {
+			log.Fatalf("%v", err)
+		}
+	}
 
-	if err := g.Update(paths); err != nil {
+	if err := u.Update(paths...); err != nil {
 		log.Fatalf("%v", err)
 	}
 }
