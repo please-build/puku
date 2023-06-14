ef191c043483f9ea0a28b617049a71946ac2f211
Jonathan Poole <jonfpoole@gmail.com>
2023-06-14 12:17:20 +0100

    Fix issue with adding same dep twice on external tests (#21)
    * Fix issue with adding same dep twice on external tests
    
    * run lint

--- generate/generate.go
+++ generate/generate.go
@@ -3,8 +3,10 @@ package generate
 import (
 	"fmt"
 	"path/filepath"
+	"strings"
 
 	"github.com/bazelbuild/buildtools/build"
+	"github.com/bazelbuild/buildtools/labels"
 	"github.com/peterebden/go-cli-init/v5/logging"
 
 	"github.com/please-build/puku/config"
@@ -252,6 +254,9 @@ func (u *Update) updateRuleDeps(conf *config.Config, rule *rule, rules []*rule, 
 			if rule.kind.IsProvided(dep) {
 				continue
 			}
+
+			dep = shorten(rule.dir, dep)
+
 			if _, ok := deps[dep]; !ok {
 				deps[dep] = struct{}{}
 			}
@@ -259,7 +264,7 @@ func (u *Update) updateRuleDeps(conf *config.Config, rule *rule, rules []*rule, 
 	}
 
 	// Add any libraries for the same package as us
-	if rule.kind.Type == kinds.Test {
+	if rule.kind.Type == kinds.Test && !rule.isExternal() {
 		pkgName, err := rulePkg(sources, rule)
 		if err != nil {
 			return err
@@ -293,6 +298,15 @@ func (u *Update) updateRuleDeps(conf *config.Config, rule *rule, rules []*rule, 
 	rule.setOrDeleteAttr("deps", depSlice)
 
 	return nil
+}
+
+// shorten will shorten lables to the local package
+func shorten(pkg, label string) string {
+	if strings.HasPrefix(label, "///") || strings.HasPrefix(label, "@") {
+		return label
+	}
+
+	return labels.Shorten(label, pkg)
 }
 
 // readRulesFromFile reads the existing build rules from the BUILD file
