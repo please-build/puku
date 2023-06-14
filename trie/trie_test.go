4212741828f5df43deea45af6de009998d64b836
Jon Poole <jpoole@thoughtmachine.net>
2023-06-14 18:32:57 +0100

    Resolve installs to the most specific one

--- trie/trie_test.go
+++ trie/trie_test.go
@@ -9,6 +9,8 @@ func TestTrie(t *testing.T) {
 	installs := map[string]string{
 		"//third_party/go:bar": "github.com/foo/bar/...",
 		"//third_party/go:baz": "github.com/foo/baz",
+		"//third_party/go:v1":  "github.com/some/module/...",
+		"//third_party/go:v2":  "github.com/some/module/v2/...",
 	}
 
 	trie := New()
@@ -20,5 +22,7 @@ func TestTrie(t *testing.T) {
 	assert.Equal(t, "//third_party/go:bar", trie.Get("github.com/foo/bar"))
 	assert.Equal(t, "//third_party/go:bar", trie.Get("github.com/foo/bar/baz"))
 	assert.Equal(t, "//third_party/go:baz", trie.Get("github.com/foo/baz"))
+	assert.Equal(t, "//third_party/go:v1", trie.Get("github.com/some/module/foo"))
+	assert.Equal(t, "//third_party/go:v2", trie.Get("github.com/some/module/v2/bar"))
 	assert.Equal(t, "", trie.Get("github.com/foo/baz/bar"))
 }
