db0cdbd3c113903c54671a1921a9f31e6fe44966
Jonathan Poole <jonfpoole@gmail.com>
2023-06-21 13:02:13 +0100

    Sort out logging setup (#43)
    * Sort out logging setup
    
    * lint

--- puku.go
+++ puku.go
@@ -5,20 +5,22 @@ import (
 	"path/filepath"
 
 	"github.com/peterebden/go-cli-init/v5/flags"
-	"github.com/peterebden/go-cli-init/v5/logging"
+	clilogging "github.com/peterebden/go-cli-init/v5/logging"
 
 	"github.com/please-build/puku/config"
 	"github.com/please-build/puku/generate"
+	"github.com/please-build/puku/logging"
 	"github.com/please-build/puku/please"
 	"github.com/please-build/puku/watch"
 	"github.com/please-build/puku/work"
 )
 
 var opts = struct {
-	Usage    string
-	LintOnly bool `long:"nowrite" description:"Prints corrections to stdout instead of formatting the files"`
-	Watch    bool `long:"watch" description:"Watch the directory"`
-	Args     struct {
+	Usage     string
+	Verbosity clilogging.Verbosity `short:"v" long:"verbosity" description:"Verbosity of output (error, warning, notice, info, debug)" default:"info"`
+	LintOnly  bool                 `long:"nowrite" description:"Prints corrections to stdout instead of formatting the files"`
+	Watch     bool                 `long:"watch" description:"Watch the directory"`
+	Args      struct {
 		Paths []string `positional-arg-name:"packages" description:"The packages to process"`
 	} `positional-args:"true"`
 }{
@@ -27,9 +29,12 @@ puku is a tool used to generate and update Go targets in build files
 `,
 }
 
-var log = logging.MustGetLogger()
+var log = logging.GetLogger()
 
 func main() {
+	flags.ParseFlagsOrDie("puku", &opts, nil)
+	logging.InitLogging(opts.Verbosity)
+
 	wd, err := os.Getwd()
 	if err != nil {
 		log.Fatalf("failed to get wd: %v", err)
@@ -48,8 +53,6 @@ func main() {
 	if err := os.Chdir(root); err != nil {
 		log.Fatalf("failed to set working dir to repo root: %v", err)
 	}
-
-	flags.ParseFlagsOrDie("puku", &opts, nil)
 
 	if opts.LintOnly && opts.Watch {
 		log.Fatalf("watch mode doesn't support --nowrite")
