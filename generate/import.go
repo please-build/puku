dcaa92c459a47c84bff1be05ccb7b6e3df730d08
Jon Poole <jpoole@thoughtmachine.net>
2023-11-23 15:42:16 +0000

    Include generated files

--- generate/import.go
+++ generate/import.go
@@ -1,7 +1,6 @@
 package generate
 
 import (
-	"go/ast"
 	"go/parser"
 	"go/token"
 	"os"
@@ -40,10 +39,7 @@ func ImportDir(dir string) (map[string]*GoFile, error) {
 		if err != nil {
 			return nil, err
 		}
-
-		if f != nil {
-			ret[info.Name()] = f
-		}
+		ret[info.Name()] = f
 	}
 
 	return ret, nil
@@ -53,9 +49,6 @@ func importFile(dir, src string) (*GoFile, error) {
 	f, err := parser.ParseFile(token.NewFileSet(), filepath.Join(dir, src), nil, parser.ImportsOnly|parser.ParseComments)
 	if err != nil {
 		return nil, err
-	}
-	if IsGenerated(f) {
-		return nil, nil
 	}
 	imports := make([]string, 0, len(f.Imports))
 	for _, i := range f.Imports {
@@ -92,33 +85,4 @@ func (f *GoFile) kindType() kinds.Type {
 		return kinds.Bin
 	}
 	return kinds.Lib
-}
-
-// IsGenerated returns whether a file is generated this is copied from
-// https://go-review.googlesource.com/c/go/+/487935/8/src/go/ast/ast.go
-func IsGenerated(file *ast.File) bool {
-	_, ok := generator(file)
-	return ok
-}
-
-func generator(file *ast.File) (string, bool) {
-	for _, group := range file.Comments {
-		for _, comment := range group.List {
-			if comment.Pos() > file.Package {
-				break // after package declaration
-			}
-			// opt: check Contains first to avoid unnecessary array allocation in Split.
-			const prefix = "// Code generated "
-			if strings.Contains(comment.Text, prefix) {
-				for _, line := range strings.Split(comment.Text, "\n") {
-					if rest, ok := strings.CutPrefix(line, prefix); ok {
-						if gen, ok := strings.CutSuffix(rest, " DO NOT EDIT."); ok {
-							return gen, true
-						}
-					}
-				}
-			}
-		}
-	}
-	return "", false
 }
